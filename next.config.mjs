1aec27c2b80eaa00ac04ddeb08e6ae1b6f38c58f
starcradle101 <dreamerdev12@gmail.com>
2024-05-08 00:17:47 +0900

    chore: Configure next.config.mjs Configure next.config.mjs to use MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+	// 기타 필요한 플러그인 추가 예정
+});
+
+export default withMDX(nextConfig);
