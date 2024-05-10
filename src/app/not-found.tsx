1e5c4ed72238abd4b2dac8168f5d8eb6bdcae110
starcradle101 <dreamerdev12@gmail.com>
2024-05-11 00:46:33 +0900

    design: Add 404 Page 404 페이지 디자인 구현

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+	return (
+		<section className='flex flex-col items-center pt-10'>
+			<h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
+				404 - 페이지 찾을 수 없음
+			</h1>
+			<p className='mb-4'>요청하신 페이지를 찾을 수 없어요</p>
+			<Link href='/' className=' bg-black text-white p-3'>
+				홈으로 돌아가기
+			</Link>
+		</section>
+	);
+}
