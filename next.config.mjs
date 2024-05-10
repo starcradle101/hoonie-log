413ece0d222690dfe5a593ad398c341cc8e8fd93
starcradle101 <dreamerdev12@gmail.com>
2024-05-11 00:25:59 +0900

    chore: Change next config next.config.mjs에서 불필요한 설정 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import createMDX from '@next/mdx';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
 };
 
-const withMDX = createMDX({
-	// 기타 필요한 플러그인 추가 예정
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
