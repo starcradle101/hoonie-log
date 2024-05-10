4c958df9ec23c9e6997524bffd58af12b9a50f43
starcradle101 <dreamerdev12@gmail.com>
2024-05-11 00:49:32 +0900

    chore: Add constants 상수 추가

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+import { join } from 'path';
+
+const BASE_DIR = process.cwd();
+export const POSTS_PATH = join(BASE_DIR, '/src/posts/blog');
+export const CRAFTS_PATH = join(BASE_DIR, '/src/posts/craft');
