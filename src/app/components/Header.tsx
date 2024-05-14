51b2c716099b178f958c9bd8dd7e2b2d9bdf2a5b
starcradle101 <dreamerdev12@gmail.com>
2024-05-14 16:28:51 +0900

    design(header): Remove BlogLogo component from header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,17 @@
 import Link from 'next/link';
-import BlogLogo from './BlogLogo';
 
 export default function Header() {
 	return (
 		<header className='flex justify-between py-8'>
-			<BlogLogo />
+			<Link className='text-2xl font-semibold' href='/'>
+				hoonie-log
+			</Link>
 			<nav className='flex gap-4'>
 				<Link className='text-base self-center font-medium' href='/blog'>
 					blog
 				</Link>
 				<Link className='text-base self-center font-medium' href='/craft'>
 					craft
-				</Link>
-				<Link className='text-base self-center font-medium' href='/about'>
-					about
 				</Link>
 			</nav>
 		</header>
