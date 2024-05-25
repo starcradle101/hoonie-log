27fff92adb3e6dd44c76c4ea12e6c24bee519c75
starcradle101 <dreamerdev12@gmail.com>
2024-05-25 16:32:27 +0900

    chore: change commitlint rules 커스텀 commitlint 사용을 위한 규칙 추가

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,24 @@
-const config = { extends: ['@commitlint/config-conventional'] };
+const config = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test',
+			],
+		],
+	},
+};
 
 export default config;
