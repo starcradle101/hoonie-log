204233ec85857a885cafcf8d05385d40672c75b9
starcradle101 <dreamerdev12@gmail.com>
2024-05-28 22:49:25 +0900

    design(Authentication): change authentication component
     인증 컴포넌트에 Link 컴포넌트를 추가해 클릭시 로그인 페이지로 이동하게 수정
    Refs: #4

--- src/app/components/Authentication.tsx
+++ src/app/components/Authentication.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import AuthenticationLogo from '@/public/icons/icon-authentication.svg';
 
 export default function Linkedin() {
 	return (
-		<Image
-			src={AuthenticationLogo}
-			width={24}
-			height={24}
-			alt='linkedin icon'
-		/>
+		<Link href='/login'>
+			<Image
+				src={AuthenticationLogo}
+				width={24}
+				height={24}
+				alt='linkedin icon'
+			/>
+		</Link>
 	);
 }
