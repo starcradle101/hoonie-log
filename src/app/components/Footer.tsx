7d66c84a007a9a8706358be55ecada99d116cb66
starcradle101 <dreamerdev12@gmail.com>
2024-05-28 23:21:05 +0900

    design(page): change markup of page compoents
    not-found 페이지와 Footer 컴포넌트에 대한 마크업 일부 수정
    Refs: #4

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import Authentication from './Authentication';
 
 export default function Footer() {
 	return (
-		<div className='flex flex-col gap-2 py-8'>
+		<footer className='flex flex-col gap-2 py-8'>
 			<div className='flex justify-center gap-4'>
 				<a href='https://github.com/starcradle101'>
 					<Github />
@@ -12,14 +12,12 @@ export default function Footer() {
 				<a href='https://www.linkedin.com/in/dreamerdev12/'>
 					<Linkedin />
 				</a>
-				<a href=''>
-					<Authentication />
-				</a>
+				<Authentication />
 			</div>
 			<span className='block break-keep text-center text-sm text-slate-500'>
 				© 2024. Hoon Kang. <br />이 페이지에는 리디주식회사에서 제공한 리디바탕
 				글꼴이 사용되어 있습니다.
 			</span>
-		</div>
+		</footer>
 	);
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function NotFound() {
 	return (
-		<section className='flex flex-col items-center pt-10'>
+		<section className='flex min-h-full flex-col items-center justify-center'>
 			<h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
 				404 - 페이지 찾을 수 없음
 			</h1>
-			<p className='mb-4'>요청하신 페이지를 찾을 수 없어요</p>
+			<p className='mb-4'>요청하신 페이지를 찾을 수 없어요 😥</p>
 			<Link href='/' className=' bg-black p-3 text-white'>
 				홈으로 돌아가기
 			</Link>
