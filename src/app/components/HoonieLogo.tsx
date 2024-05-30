454bfa66d85a7fa631efaf8eb15c5975cd07ec56
starcradle101 <dreamerdev12@gmail.com>
2024-05-31 02:19:24 +0900

    design(HoonieLogo): create HoonieLogo component
    블로그에서 사용할 HoonieLogo 컴포넌트 구현
    Refs: #6

---
+++ src/app/components/HoonieLogo.tsx
@@ -0,0 +1,6 @@
+import BlogLogo from '@/public/icons/logo.png';
+import Image from 'next/image';
+
+export default function HoonieLogo() {
+	return <Image src={BlogLogo} width={48} height={48} alt='clock icon' />;
+}
