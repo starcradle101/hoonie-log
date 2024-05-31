be226e32885760d15e7b9dcb2ff14a0f4131abae
starcradle101 <dreamerdev12@gmail.com>
2024-05-31 14:51:44 +0900

    remove(icons): remove icons can be replaced with heroicons
    heroicons로 대체될 수 있는 아이콘 컴포넌트 삭제
    Refs: #6

--- src/app/components/Clock.tsx
+++
@@ -1,6 +0,0 @@
-import ClockIcon from '@/public/icons/icon-clock.svg';
-import Image from 'next/image';
-
-export default function Clock() {
-	return <Image src={ClockIcon} width={16} height={16} alt='clock icon' />;
-}
--- src/app/components/Mail.tsx
+++
@@ -1,6 +0,0 @@
-import Image from 'next/image';
-import MailIcon from '@/public/icons/icon-mail.svg';
-
-export default function Mail() {
-	return <Image src={MailIcon} width={24} height={24} alt='email icon' />;
-}
