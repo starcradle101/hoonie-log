1033da59ddc311c23433751edb9f9b344da0e421
starcradle101 <dreamerdev12@gmail.com>
2024-07-01 17:07:52 +0900

    design(sidenav): 사이드바 UI 구현
    사이드바 UI 구현 및 일부 변경
    Related to: #6

--- src/app/components/NavLinks.tsx
+++ src/app/components/NavLinks.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { HomeIcon, PlusIcon, FolderIcon } from '@heroicons/react/24/solid';
+import { BarChart3Icon, FilePlusIcon, LibraryBigIcon } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const links = [
-	{ name: '홈', href: '/dashboard', icon: HomeIcon },
+	{ name: '블로그 통계', href: '/dashboard', icon: BarChart3Icon },
 	{
 		name: '새 글 작성',
 		href: '/dashboard/write',
-		icon: PlusIcon,
+		icon: FilePlusIcon,
 	},
-	{ name: '작성 글 목록', href: '/dashboard/posts', icon: FolderIcon },
+	{ name: '글 목록', href: '/dashboard/posts', icon: LibraryBigIcon },
 ];
 
 export default function NavLinks() {
@@ -21,11 +21,17 @@ export default function NavLinks() {
 		<>
 			{links.map((link) => {
 				const LinkIcon = link.icon;
+				const isActive = pathname === link.href;
+
 				return (
 					<Link
 						key={link.name}
 						href={link.href}
-						className='flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'
+						className={`flex h-12 grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 ${
+							isActive
+								? 'bg-green-600 text-white'
+								: 'bg-gray-50 text-gray-800 hover:bg-green-100 hover:text-green-600'
+						}`}
 					>
 						<LinkIcon className='w-6' />
 						<p className='hidden md:block'>{link.name}</p>
--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -1,12 +1,12 @@
 import NavLinks from './NavLinks';
 import Link from 'next/link';
 import { signout } from '@/src/utils/supabase/actions';
-import { PowerIcon } from '@heroicons/react/24/solid';
-import HoonieLogo from './HoonieLogo';
+import { PowerIcon } from 'lucide-react';
+import HoonieLogo from './icon/HoonieLogo';
 
 export default function SideNav() {
 	return (
-		<nav className='flex h-full flex-col px-2 py-4'>
+		<nav className='flex shrink-0 flex-col p-2 md:h-screen'>
 			<Link
 				className='flex h-20 items-end justify-start rounded-md bg-gray-900 p-4 md:mb-2 md:h-40 '
 				href='/'
@@ -15,11 +15,11 @@ export default function SideNav() {
 				<span className=' text-3xl font-semibold text-white'>hoonie-log</span>
 			</Link>
 			<div className='flex grow space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
-				<div className='flex h-auto w-full grow rounded-md bg-gray-50 md:block'>
+				<div className='mt-2 flex h-auto w-full flex-1 rounded-md bg-gray-50 md:mt-0 md:block'>
 					<NavLinks />
 				</div>
 
-				<div className='flex items-center justify-center rounded-md bg-gray-50 hover:bg-sky-100 hover:text-blue-600 md:block'>
+				<div className='flex items-center justify-center rounded-md bg-gray-50 hover:bg-green-100 hover:text-green-600 md:block'>
 					<form>
 						<button
 							formAction={signout}
