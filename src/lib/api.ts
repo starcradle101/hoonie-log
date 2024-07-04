7f0b42fc26d683dd1394d6ae8a604f748936618d
starcradle101 <dreamerdev12@gmail.com>
2024-07-04 22:27:11 +0900

    comment(api): 게시글 api 주석처리
    Supabase를 사용한 CRUD 기능 구현 시 사용할 api 변경을 위한 주석처리
    Related to: #6

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,77 +1,81 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import { join } from 'path';
 import dayjs from 'dayjs';
-import readingTime from 'reading-time';
-import { unified } from 'unified';
-import remarkParse from 'remark-parse';
-import remarkGfm from 'remark-gfm';
-import remarkGithub from 'remark-github';
-import remarkBreaks from 'remark-breaks';
-import remarkRehype from 'remark-rehype';
-import rehypeFormat from 'rehype-format';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypePrettyCode from 'rehype-pretty-code';
-import rehypeStringify from 'rehype-stringify';
 import { Post, PostAbstract } from '@/src/interfaces/post';
 
-// 카테고리 작성 글 슬러그 목록을 불러오기
-export function getPostSlugsFrom(postDirectory: string) {
-	return fs.readdirSync(postDirectory);
-}
+// 게시글 데이터 생성하기
+export const createPost = (
+	title: string,
+	description: string,
+	content: object
+): Post => {
+	const slug = encodeURIComponent(title);
+
+	const createdAt = dayjs().locale('ko').format('YYYY년 MM월 DD일');
+	const updatedAt = createdAt;
+
+	const postData: Post = {
+		slug,
+		title,
+		description,
+		content,
+		createdAt,
+		updatedAt,
+	};
+
+	return postData;
+};
 
 // 게시글 상세 정보 불러오기
-export function getPostBySlug(slug: string, postDirectory: string) {
-	const realSlug = slug.replace(/\.md$/, '');
-	const fullPath = join(postDirectory, `${realSlug}.md`);
-	const postContents = fs.readFileSync(fullPath, 'utf8');
-	const { data, content } = matter(postContents);
-	const readingMinutes = Math.ceil(readingTime(content).minutes);
-	const dateString = dayjs(data.date).locale('ko').format('YYYY년 MM월 DD일');
-	return {
-		...data,
-		readingMinutes,
-		dateString,
-		slug: realSlug,
-		content,
-	} as Post;
-}
+// export function getPostBySlug(slug: string, postDirectory: string) {
+// 	const realSlug = slug.replace(/\.md$/, '');
+// 	const fullPath = join(postDirectory, `${realSlug}.md`);
+// 	const postContents = fs.readFileSync(fullPath, 'utf8');
+// 	const { data, content } = matter(postContents);
+// 	const readingMinutes = Math.ceil(readingTime(content).minutes);
+// 	const dateString = dayjs(data.date).locale('ko').format('YYYY년 MM월 DD일');
+// 	return {
+// 		...data,
+// 		readingMinutes,
+// 		createdAt,
+//     updatedAt
+// 		slug: realSlug,
+// 		content,
+// 	} as Post;
+// }
 
 // 포스트 요약 정보 (url, slug, description)만 불러오기
-export function getPostAbstracts(postDirectory: string) {
-	const files = getPostSlugsFrom(postDirectory);
-	const postAbstracts: PostAbstract[] = [];
+// export function getPostAbstracts(postDirectory: string) {
+// 	const files = getPostSlugsFrom(postDirectory);
+// 	const postAbstracts: PostAbstract[] = [];
 
-	files.forEach((file) => {
-		const post = getPostBySlug(file, postDirectory);
-		const { title, description, dateString, slug } = post;
+// 	files.forEach((file) => {
+// 		const post = getPostBySlug(file, postDirectory);
+// 		const { title, description, dateString, slug } = post;
 
-		postAbstracts.push({
-			title,
-			description,
-			dateString,
-			slug,
-		});
-	});
+// 		postAbstracts.push({
+// 			title,
+// 			description,
+// 			dateString,
+// 			slug,
+// 		});
+// 	});
 
-	return postAbstracts;
-}
+// 	return postAbstracts;
+// }
 
 // 마크다운 콘텐츠를 HTML 파일로 변환하기
-export async function markdownToHTML(content: string) {
-	const htmlContent = await unified()
-		.use(remarkParse)
-		.use(remarkGfm)
-		.use(remarkGithub)
-		.use(remarkBreaks)
-		.use(remarkRehype)
-		.use(rehypeFormat)
-		.use(rehypeSlug)
-		.use(rehypeAutolinkHeadings)
-		.use(rehypePrettyCode)
-		.use(rehypeStringify)
-		.process(content);
+// export async function markdownToHTML(content: string) {
+// 	const htmlContent = await unified()
+// 		.use(remarkParse)
+// 		.use(remarkGfm)
+// 		.use(remarkGithub)
+// 		.use(remarkBreaks)
+// 		.use(remarkRehype)
+// 		.use(rehypeFormat)
+// 		.use(rehypeSlug)
+// 		.use(rehypeAutolinkHeadings)
+// 		.use(rehypePrettyCode)
+// 		.use(rehypeStringify)
+// 		.process(content);
 
-	return htmlContent.toString();
-}
+// 	return htmlContent.toString();
+// }
