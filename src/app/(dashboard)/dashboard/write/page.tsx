21b75e381ec6c498b34eb13840fa64daf907ebf7
starcradle101 <dreamerdev12@gmail.com>
2024-07-04 22:37:57 +0900

    feat(write): 글 작성 페이지 데이터 생성 구현
    글 작성 페이지에서 작성 버튼을 클릭할 경우 올바른 데이터가 출력되는지 확인
    Related to: #6

--- src/app/(dashboard)/dashboard/write/page.tsx
+++ src/app/(dashboard)/dashboard/write/page.tsx
@@ -3,24 +3,39 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Tiptap from '@/src/app/components/Tiptap';
 import Modal from '@/src/app/components/Modal';
+import { createPost } from '@/src/lib/api';
 
-interface FormValues {
+interface ContentType {
 	title: string;
+	description: string;
 	body: string;
 }
 
 export default function Page() {
 	const [title, setTitle] = useState<string>('');
-	const [content, setContent] = useState<string>('');
+	const [description, setDescription] = useState<string>('');
+	const [content, setContent] = useState<object>({});
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 	const router = useRouter();
 
-	const handleTitleChange = (reason: any) => {
-		setTitle(reason);
+	// 여기에 supabase client로 content 불러와서 추가
+
+	const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.target.value);
 	};
 
-	const handleContentChange = (reason: any) => {
+	const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setDescription(e.target.value);
+	};
+
+	const handleContentChange = (reason: object) => {
 		setContent(reason);
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		const newPost = createPost(title, description, content);
+		console.log(newPost);
 	};
 
 	const openModal = () => {
@@ -36,12 +51,14 @@ export default function Page() {
 		router.back();
 	};
 
+	console.log({ title, description, content });
+
 	return (
 		<div className='m-auto flex h-full flex-col md:max-w-3xl'>
 			<h1 className='my-8 text-2xl font-semibold md:text-4xl'>
 				새 글 작성하기
 			</h1>
-			<form className='mt-4 flex flex-1 flex-col'>
+			<form className='mt-4 flex flex-1 flex-col' onSubmit={handleSubmit}>
 				<label htmlFor='title'>
 					<span>제목</span>
 					<span className='text-red-500'>*</span>
@@ -49,7 +66,21 @@ export default function Page() {
 				<input
 					type='text'
 					id='title'
-					className='mb-4 mt-2 block w-full resize-none rounded border-2 p-2 focus:outline-blue-500'
+					placeholder='작성할 글의 제목을 입력해주세요.'
+					className='mb-4 mt-2 block w-full resize-none rounded border-2 p-2 focus:outline-green-500'
+					onChange={handleTitleChange}
+				/>
+
+				<label htmlFor='description'>
+					<span>설명</span>
+					<span className='text-red-500'>*</span>
+				</label>
+				<input
+					type='text'
+					id='description'
+					placeholder='작성할 글에 대한 간단한 설명을 입력해주세요.'
+					className='mb-4 mt-2 block w-full resize-none rounded border-2 p-2 focus:outline-green-500'
+					onChange={handleDescriptionChange}
 				/>
 
 				<label htmlFor='body'>
@@ -59,7 +90,7 @@ export default function Page() {
 				<Tiptap
 					id='body'
 					content={content}
-					onChange={(newContent: string) => handleContentChange(newContent)}
+					onChange={(newContent: object) => handleContentChange(newContent)}
 				/>
 
 				<div className='my-4 flex justify-end gap-2'>
@@ -84,10 +115,11 @@ export default function Page() {
 			<Modal
 				isOpen={isModalOpen}
 				onClose={closeModal}
-				onExit={handleExit}
+				onConfirm={handleExit}
 				title='작성 취소'
+				confirmButtonText='나가기'
 			>
-				<p className='mb-4 text-sm leading-relaxed text-slate-600 md:text-base'>
+				<p>
 					입력한 내용을 저장하지 않았습니다.
 					<br /> &apos;나가기&apos;를 누르면 작성한 내용이 사라집니다.
 				</p>
