72dbdf5c79ed60aead18fc0e5502254735a8be50
starcradle101 <dreamerdev12@gmail.com>
2024-07-04 22:35:20 +0900

    feat(sidenav): 사이드바 컴포넌트 구현
    Related to: #6

--- src/app/components/NavLinks.tsx
+++ src/app/components/NavLinks.tsx
@@ -29,8 +29,8 @@ export default function NavLinks() {
 						href={link.href}
 						className={`flex h-12 grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 ${
 							isActive
-								? 'bg-green-600 text-white'
-								: 'bg-gray-50 text-gray-800 hover:bg-green-100 hover:text-green-600'
+								? 'bg-green-500 text-white'
+								: 'bg-gray-50 text-gray-800 hover:bg-green-100 hover:text-green-500'
 						}`}
 					>
 						<LinkIcon className='w-6' />
--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -8,18 +8,19 @@ export default function SideNav() {
 	return (
 		<nav className='flex shrink-0 flex-col p-2 md:h-screen'>
 			<Link
-				className='flex h-20 items-end justify-start rounded-md bg-gray-900 p-4 md:mb-2 md:h-40 '
+				className='flex h-20 items-end justify-start rounded-md bg-green-900 p-4 md:mb-2 md:h-40 '
 				href='/'
 			>
 				<HoonieLogo />
 				<span className=' text-3xl font-semibold text-white'>hoonie-log</span>
 			</Link>
+
 			<div className='flex grow space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
-				<div className='mt-2 flex h-auto w-full flex-1 rounded-md bg-gray-50 md:mt-0 md:block'>
+				<div className='mt-2 flex h-auto w-full flex-1 rounded-md bg-gray-50 md:mt-0 md:flex-col'>
 					<NavLinks />
 				</div>
 
-				<div className='flex items-center justify-center rounded-md bg-gray-50 hover:bg-green-100 hover:text-green-600 md:block'>
+				<div className='mt-2 flex items-center justify-start rounded-md bg-gray-50 hover:bg-green-100 hover:text-green-600 md:mt-0'>
 					<form>
 						<button
 							formAction={signout}
