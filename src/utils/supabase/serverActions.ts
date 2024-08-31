b221bce1365f620f04a357b9cf055570fdc1c23b
starcradle101 <dreamerdev12@gmail.com>
2024-08-31 13:00:59 +0900

    refactor: migrate supabase util functions to server actions
    다음과 같은 함수들을 서버 액션으로 이동
    - fetchPostAbstracts
    - fetchLatestPost
    - getPostSlugs
    - getPostFromSlug
    Related to: #23

--- src/utils/supabase/serverActions.ts
+++ src/utils/supabase/serverActions.ts
@@ -39,3 +39,90 @@ export async function getUserData() {
 
 	return user;
 }
+
+export async function fetchPostAbstracts(
+	page: number,
+	itemsPerPage: number,
+	query?: string
+) {
+	const supabase = createClient();
+	const start = (page - 1) * itemsPerPage;
+	const end = start + itemsPerPage - 1;
+
+	let queryBuilder = supabase.from('posts').select('*', { count: 'exact' });
+
+	if (query && query.trim() !== '') {
+		queryBuilder = queryBuilder
+			.ilike('title', `%${query}%`)
+			.or(`description.ilike.%${query}%`);
+	}
+
+	let { count, error: countError } = await queryBuilder;
+	if (countError) {
+		console.error(countError);
+		return { posts: [], totalPosts: 0 };
+	}
+
+	queryBuilder = queryBuilder.range(start, end);
+	let { data: posts, error } = await queryBuilder;
+
+	if (error) {
+		console.error(error);
+		return { posts: [], totalPosts: 0 };
+	}
+
+	return { posts: posts || [], totalPosts: count || 0 };
+}
+
+export async function fetchLatestPost() {
+	const supabase = createClient();
+	const { data, error } = await supabase
+		.from('posts')
+		.select('*')
+		.order('created_at', { ascending: false })
+		.limit(1)
+		.single();
+
+	if (error) {
+		console.error(error);
+		return null;
+	}
+
+	return data;
+}
+
+export const getPostSlugs = async () => {
+	const supabase = createClient();
+	const { data, error } = await supabase.from('posts').select('slug');
+
+	if (error) {
+		console.error('오류가 발생했습니다:', error);
+		return null;
+	}
+
+	return data;
+};
+
+export const getPostFromSlug = async (slug: string) => {
+	const supabase = createClient();
+	const { data, error } = await supabase
+		.from('posts')
+		.select('*')
+		.eq('slug', slug)
+		.limit(1)
+		.maybeSingle();
+
+	console.log('Data fetched for slug:', slug, data);
+
+	if (error) {
+		console.error('Error fetching post:', error.message);
+		return null;
+	}
+
+	if (!data) {
+		console.error('No data found for slug:', slug);
+		return null;
+	}
+
+	return data;
+};
