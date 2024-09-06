dc6bbc3f2bf73cad3e7c635688645b2e879b8f52
starcradle101 <dreamerdev12@gmail.com>
2024-09-07 00:13:38 +0900

    design: change tailwind config
    next-themes 사용을 위한 tailwind.config.ts 파일 클래스 추가
    Related to: #25

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+	darkMode: 'class',
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,6 +9,7 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			colors: { 'dark-theme': '#121212' },
 			typography: {
 				DEFAULT: {
 					css: {
