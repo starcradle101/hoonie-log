43d43fd661fc97ac2d99ea1e8ec1b4b5e7a4c563
starcradle101 <dreamerdev12@gmail.com>
2024-09-08 10:50:07 +0900

    fix: og image
    Related to: #37

--- src/app/(blog)/blog/[slug]/page.tsx
+++ src/app/(blog)/blog/[slug]/page.tsx
@@ -3,33 +3,6 @@ import PostBody from '../../../components/PostBody';
 import Giscus from '../../../components/Giscus';
 import dayjs from 'dayjs';
 import { getPostFromSlug } from '@/src/utils/supabase/serverActions';
-import { Metadata } from 'next';
-
-export async function generateMetadata({
-	params,
-}: {
-	params: { slug: string };
-}): Promise<Metadata> {
-	const post = await getPostFromSlug(params.slug);
-
-	if (!post) {
-		return {
-			title: 'Post not found',
-			description: 'No post found with this slug.',
-		};
-	}
-
-	return {
-		title: post.title,
-		description: post.description,
-		openGraph: {
-			title: post.title,
-			description: post.description,
-			url: `https://hoonie-log.vercel.app/blog/${params.slug}`,
-			// images: post.ogImage || 'https://your-site.com/default-og-image.png',
-		},
-	};
-}
 
 export default async function Page({ params }: { params: { slug: string } }) {
 	const post = await getPostFromSlug(decodeURIComponent(params.slug));
