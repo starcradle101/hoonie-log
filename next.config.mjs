d68fcdd8f13d19a042a9953863772bdeca10f1d5
starcradle101 <dreamerdev12@gmail.com>
2024-09-19 17:28:40 +0900

    fix: supabase image 오류 해결
    * next.config.mjs에 supabase storage 관련 remote pattern 추가
    Related to: #50

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'sryzicafffncxapkoqch.supabase.co',
+				port: '',
+				pathname: '/storage/v1/object/public/**',
+			},
+		],
+	},
 	pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
 };
 
