e4ea1028f499939c00ce1727fc0e67d9b37a0cfc
starcradle101 <dreamerdev12@gmail.com>
2024-11-03 22:02:06 +0900

    test: next 15 업데이트에 따른 params 비동기 처리
    Related to: #53

--- src/app/(dashboard)/dashboard/posts/[slug]/page.tsx
+++ src/app/(dashboard)/dashboard/posts/[slug]/page.tsx
@@ -5,17 +5,21 @@ import { getPostFromSlug } from '@/src/utils/supabase/serverActions';
 type Post = {
 	title: string;
 	created_at: string;
-	content: object; // 수정된 부분
+	content: object;
 };
 
-export default async function Page({ params }: { params: { slug: string } }) {
-	const post: Post | null = await getPostFromSlug(
-		decodeURIComponent(params.slug)
-	);
+export default async function Page({
+	params,
+}: {
+	params: Promise<{ slug: string }>;
+}) {
+	const { slug } = await params; // params를 비동기적으로 처리
+	const post: Post | null = await getPostFromSlug(decodeURIComponent(slug));
 
 	if (!post) {
 		return <div>Post not found</div>;
 	}
+
 	return (
 		<section className='m-auto py-6 md:max-w-3xl md:py-12'>
 			<PostHeader title={post.title} created_at={post.created_at} />
